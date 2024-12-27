b5523fa36a51793f775267f4d3dcf98d03e892fe
박시현 <dkdi4224@naver.com>
2024-12-28 01:56:19 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/ssafy/four_paws/FourPawsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.four_paws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourPawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourPawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/four_paws/FourPawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.four_paws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourPawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
